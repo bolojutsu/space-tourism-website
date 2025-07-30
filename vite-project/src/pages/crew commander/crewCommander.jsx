518216f5b5c56bfb62d01dc21ae7060ef3349f2d
Devin <devironi@Ravishings-MacBook-Pro.local>
2025-07-30 14:22:40 -0400

    fixed css and added navigation functionality to for crew commander

--- vite-project/src/pages/crew commander/crewCommander.jsx
+++ vite-project/src/pages/crew commander/crewCommander.jsx
@@ -1,6 +1,18 @@
 import crewData from '../../data.json'
 import './crewCommander.css'
+import { useState } from 'react';
 function CrewCommander({ setCurrentPage }) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const currentCrew = crewData.crew[currentIndex];
+
+    const nextCrew = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % crewData.crew.length);
+    }
+
+    const prevCrew = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + crewData.crew.length) % crewData.crew.length);
+    }
+
     return (
         <div className="background">
             <nav className="nav-bar">
@@ -21,26 +33,35 @@ function CrewCommander({ setCurrentPage }) {
             </nav>
 
             <div className="crew-selection">
-                {crewData.crew.map((crewMember, index) => (
-                    <div key={index} className="crew-member">
-                        <div className="crew-details">
-                            <h2>{crewMember.role}</h2>
-                            <h3>{crewMember.name}</h3>
-                            <p>{crewMember.bio}</p>
-                        </div>
-
-                        <div className="crew-image">
-                            <img src={crewMember.images.webp} alt={crewMember.name}
-                                className="crew-img"
-                            />
-                        </div>
+                <h1>02 MEET YOUR CREW</h1>
+                <div className="crew-member">
+                    <div className="crew-details">
+                        <h2 className="crew-role">{currentCrew.role}</h2>
+                        <h3 className='crew-name'>{currentCrew.name}</h3>
+                        <p className='crew-bio'>{currentCrew.bio}</p>
                     </div>
-                ))}
-
+                </div>
+                <div className='crew-image'>
+                    <img src={currentCrew.images.webp} alt={currentCrew.name} className='crew-image' />
+                </div>
+                <div className='crew-dots'>
+                    {crewData.crew.map((_, index) => {
+                        let className;
+                        if (index === 0) {
+                            className = 'active-dot';
+                        } else {
+                            className = 'dot';
+                        }
+                        return <span key={index} className={className}></span>
+                    })}
+                </div>
+                <div className='crew-navigation'>
+                    <button onClick={prevCrew}>Previous</button>
+                    <button onClick={nextCrew}>Next</button>
+                </div>
             </div>
-
         </div>
-    )
+    );
 }
 
 export default CrewCommander
