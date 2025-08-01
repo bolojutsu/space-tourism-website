4631424782f89ee3e945c8a870c8839377b9e658
bolojutsu <200mshinobi@gmail.com>
2025-08-01 12:25:09 -0400

    improved styling for home page

--- vite-project/src/pages/home/home.jsx
+++ vite-project/src/pages/home/home.jsx
@@ -12,7 +12,7 @@ function Home({ setCurrentPage }) {
                                 SPACE
                             </p>
                         </div>
-                        <p className="home-sub-header">Let's face it: if you ant to go to space, you might as well genuenly go to outer space and not over kindof on the edge of it. well sit back and relax because we'll give you a truly out of this world experience!</p>
+                        <p className="home-sub-header">Let's face it: if you ant to go to space, you might as well genuenly go to outer space and not over kind of on the edge of it. well sit back and relax because we'll give you a truly out of this world experience!</p>
                     </div>
                     <div className="home-explore">Explore</div>
                 </div>
